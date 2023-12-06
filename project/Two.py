b52ca66a892f4af3ca54b1d6fb017ef238dd827b
April Mariko Salazar <99777447+aprilmarikosalazar@users.noreply.github.com>
2023-12-06 14:09:42 -0800

    Add files via upload

---
+++ project/Two.py
@@ -0,0 +1,12 @@
+"""This is my submission for Assignment Two."""
+
+
+def main():
+    """Prompt user to enter name, then greet them and state activity."""
+    name = input("Please enter your name: ")
+    string = f"Hi {name}, let's explore some historical temperatures."
+    print(string)
+
+
+if __name__ == "__main__":
+    main()
